05a6234dd3ada4760e689af90aba088d6beec5ca
rubywhinery <rubywhinery@gmail.com>
2022-03-30 04:07:10 +0000

    add out

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  let nextConfig;
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    nextConfig = {
+      reactStrictMode: true,
+      env: {
+        PREFIX: ''
+      }
+    };
+  } else {
+    const url = "/odin-recipes";
+    nextConfig = {
+      reactStrictMode: true,
+      basePath: `${url}`,
+      assetPrefix: `${url}/`,
+      env: {
+        PREFIX: `${url}`
+      }
+    };
+  }
+
+  return nextConfig;
+}
